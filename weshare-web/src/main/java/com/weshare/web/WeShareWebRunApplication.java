2d1d911a49df90fd7748bed68adccb5af0af8d2e
S1m0n_178 <xnz2669718508@foxmail.com>
2025-03-17 00:14:38 +0800

    账号表增删改查实现

--- weshare-web/src/main/java/com/weshare/web/WeShareWebRunApplication.java
+++ weshare-web/src/main/java/com/weshare/web/WeShareWebRunApplication.java
@@ -1,10 +1,13 @@
 package com.weshare.web;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = {"com.weshare"},exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.weshare"})
+@MapperScan(basePackages= {"com.weshare.mappers"})
 public class WeShareWebRunApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeShareWebRunApplication.class,args);
